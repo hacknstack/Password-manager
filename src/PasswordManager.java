f9cc7e7554a8de33d7ad5fe4ffc9c32956014f03
hacknstack <ryan.mehenni@epfl.ch>
2024-08-16 10:00:33 +0200

    added AES encrypt. and decrypt. + using a secure hashing technique now !

--- src/PasswordManager.java
+++ src/PasswordManager.java
@@ -1,4 +1,11 @@
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.Provider;
+import java.security.Provider.Service;
+import java.security.Security;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Set;
 
 public class PasswordManager {
 	private int hashcode;
@@ -7,11 +14,13 @@ public class PasswordManager {
 	public PasswordManager(String GlobalPasscode) {
 		this.localPasscodes = new HashMap<>();
 		this.hashcode = hashFun(GlobalPasscode);
+		
 	}
 	public void addLocalPasscode(String website,String localPasscode) {
 		localPasscodes.put(website,localPasscode);
 	}
 	public String unveil(String GlobalPasscode,String website) {
+		System.out.println(hashFun(GlobalPasscode));
 		if (hashFun(GlobalPasscode)==hashcode){
 			if (localPasscodes.containsKey(website)){
 				return localPasscodes.get(website);
@@ -28,7 +37,16 @@ public class PasswordManager {
 		}
 		return false;
 	}
-	private int hashFun(String s) {
+	static int hashFun(String s) {
+		Provider[] array = Security.getProviders();
+		for (Provider p : array){
+			System.out.println(p.getName()+ " //"+ p.getInfo()+ "&&& ");
+			Iterator<Service> se = p.getServices().iterator();
+			while(se.hasNext()){
+				System.out.println("            "+se.next().getAlgorithm());
+			}
+		}
+		s.getBytes(StandardCharsets.UTF_8);
 		return s.hashCode();
 	}
 }
